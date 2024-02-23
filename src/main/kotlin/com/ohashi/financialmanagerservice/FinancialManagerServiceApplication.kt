2ea97988a7d0e3717d04b0bf750cce17aa03d951
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-02-23 00:18:23 -0300

    chore: project structure

---
+++ src/main/kotlin/com/ohashi/financialmanagerservice/FinancialManagerServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.ohashi.financialmanagerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialManagerServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialManagerServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/ohashi/financialmanagerservice/FinancialManagerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ohashi.financialmanagerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinancialManagerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
